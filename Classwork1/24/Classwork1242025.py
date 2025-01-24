b76301a5ad19a3d1bee0d26a682f60fb7a442842
DThompson359 <dsthompson23@coe.edu>
2025-01-24 20:45:03 +0000

    Corrections good

---
+++ Classwork1/24/Classwork1242025.py
@@ -0,0 +1,54 @@
+# x = 5
+# y = x
+# x +=3
+# print(x)
+# print(y)
+
+# def somefunc(x):
+#     y = x
+#     x = 3
+#     return x + y
+# x = 5
+# z = somefunc(x)
+# print(x)
+# print(y)
+# print(z)
+
+# num1 = int(input("what is number 1:"))
+# num2 = int(input("what is number 2:"))
+# print(num1+num2)
+
+# print(14 % 5)
+# print(14 // 5)
+# print(10,000*2)
+
+# def inch2cm(inch):
+#     cm = inch * 2.54
+#     return cm
+
+# def inch2cms(inch):
+#     return Inch *2.54
+
+
+# def getTip(bill,tipRate = .15):
+#     tip = bill*tipRate
+#     return bill + tip
+
+# print(getTip(20))
+# print(getTip(10,.2))
+
+# num = 3.378
+# print(f"You owe ${num:.2f}")
+
+# def lbs2kg(lbs):
+#     return (lbs * .453592)
+
+# lbs2kg(100)
+
+def area_of_triangle(base, height):
+    return 0.5 * base * height
+
+
+base = 3
+height = 5
+print(f"The area of the triangle is {area_of_triangle(base, height)}")
